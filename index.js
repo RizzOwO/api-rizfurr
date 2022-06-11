6614602fb74e28b910abf1b8e73ae54ef4d07217
RizzOwO <106405046+RizzOwO@users.noreply.github.com>
2022-06-12 05:00:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,21 @@
-const { 
-	otakudesu,
-	covid,
-	ongoing,
-	komiku,
-	tebakgambar,
-	surah,
-	sholat,
-	lirik,
-	chara,
-	wattpad,
-	playstore,
-	linkwa,
-	pinterest,
-	igdl,
-	igstory,
-	igstalk,
-	twitter,
-	fbdown,
-	ttdownloader, 
-	ytMp4,
-	ytMp3,
-	ytPlay
-} = require('./modules/function')
+const { version, author } = require("./package.json");
+const downloader = require('./modules/downloader')
 
-module.exports.otakudesu = otakudesu
-module.exports.covid = covid
-module.exports.ongoing = ongoing
-module.exports.komiku = komiku
-module.exports.tebakgambar = tebakgambar
-module.exports.surah = surah
-module.exports.sholat = sholat
-module.exports.lirik = lirik
-module.exports.chara = chara
-module.exports.wattpad = wattpad
-module.exports.playstore = playstore
-module.exports.linkwa = linkwa
-module.exports.pinterest = pinterest
-module.exports.igdl = igdl
-module.exports.igstory = igstory
-module.exports.igstalk = igstalk
-module.exports.twitter = twitter
-module.exports.fbdown = fbdown
-module.exports.ttdownloader = ttdownloader
-module.exports.ytMp4 = ytMp4
-module.exports.ytMp3 = ytMp3
-module.exports.ytPlay = ytPlay
-
+module.exports = {
+  version,
+  author,
+  downloader: {
+    igdl,
+    igstory,
+    igstalk,
+    fbdown,
+    ttdownloader,
+    soundcloud,
+    soundcloud2, 
+    ytMp4,
+	  ytMp3,
+	  ytMp4,
+	  ytPlay,
+	  ytPlayVid
+  }
+};
